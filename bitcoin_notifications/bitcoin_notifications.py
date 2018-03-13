c71f8478337ac11fba196006ac4170bdda84ed8a
wildlyclassyprince <wildlyclassyprince@users.noreply.github.com>
2018-03-13 15:31:06 +0200

    Update bitcoin_notifications.py

--- bitcoin_notifications/bitcoin_notifications.py
+++ bitcoin_notifications/bitcoin_notifications.py
@@ -1,23 +1,17 @@
 # coding: utf-8
 
-from __future__ import division
-from __future__ import print_function
-
 # We want to get the latest price from the Coinmarketcap API.
 
-# The suspects ...
+# The usual suspects ...
 import requests
 import time
-
-# The accomplices ...
 from datetime import datetime
 
 BITCOIN_API_URL = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
 IFTTT_WEBHOOKS_URL = 'https://maker.ifttt.com/trigger/bitcoin_emergency_alert/with/key/vRNOSa96SSQO8fcIu1Tto'
 
 def get_latest_bitcoin_price():
-    response = requests.get(BITCOIN_API_URL)
-    response_json = response.json()
+    response, response_json = requests.get(BITCOIN_API_URL), response.json()
     # Convert the price to a floating point number
     return float(response_json[0]['price_usd'])
 
@@ -33,13 +27,11 @@ def format_bitcoin_history(bitcoin_history):
     rows = []
     for bitcoin_price in bitcoin_history:
         # Formats the date into a string: 'day.month.year hour:minutes'
-        date = bitcoin_price['date'].strftime('%d.%m.%Y %H:%M')
-        price = bitcoin_price['price']
+        date, price = bitcoin_price['date'].strftime('%d.%m.%Y %H:%M'), bitcoin_price['price']
         # <b> (bold) tag creates bolded text
         # 24.02.2018 15:09: $<b>10123.4</b>
         row = '{}: $<b>{}</b>'.format(date, price)
         rows.append(row)
-
     # Use a <br> (break) tag to create a new line.
     # Join the rows delimited by <br> tag: row1<br>row2<br>row3
     return '<br>'.join(rows)
@@ -49,21 +41,18 @@ BITCOIN_PRICE_THRESHOLD = 10000
 def main():
     bitcoin_history = []
     while True:
-        price = get_latest_bitcoin_price()
-        date = datetime.now()
+        price, date = get_latest_bitcoin_price(), datetime.now()
         bitcoin_history.append({'date': date, 'price': price})
 
         # Send an emergency notification
         if price < BITCOIN_PRICE_THRESHOLD:
             post_ifttt_webhook('bitcoin_emergency_alert', price)
-
         # Send a Telegram notification.
         # Once we have 5 items in our bitcoin_history, send an update.
         if (len(bitcoin_history) == 5):
             post_ifttt_webhook('bitcoin_price_update', format_bitcoin_history(bitcoin_history))
             # Reset the history
             bitcoin_history = []
-
         # Sleet for 5 minutes
         time.sleep(5 * 60)
 
